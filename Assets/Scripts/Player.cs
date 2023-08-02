66e20a08939a1fae376ab5d5802972fc62c4da2d
IgnacioGameolay <ireyestoledo8@gmail.com>
2023-08-01 22:14:02 -0400

    Player movement system added!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+    Vector3 moveDirection;
+    [SerializeField] float speed = 20f;
+
+    private void ReadInput()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        moveDirection.x = h;
+        moveDirection.y = v;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        ReadInput();
+        transform.position += moveDirection * speed * Time.deltaTime; 
+    }
+}
